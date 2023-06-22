d00cd0307042f2e93724b555b1f0fde0ea9249c8
manjesh453 <rayamajhimanjesh41@gmail.com>
2023-06-22 16:09:51 +0545

    Added secutity with some configuration for user login

---
+++ src/main/java/com/manjesh/blog/security/CustomUserDetailService.java
@@ -0,0 +1,22 @@
+package com.manjesh.blog.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.manjesh.blog.entities.User;
+import com.manjesh.blog.exceptions.ResourceNotFoundException;
+import com.manjesh.blog.repositories.UserRepo;
+
+public class CustomUserDetailService implements UserDetailsService{
+   @Autowired
+	private UserRepo userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user=this.userRepo.findByEmail(username).orElseThrow(()->new ResourceNotFoundException("User", "Email :"+username, 0));
+		return null;
+	}
+
+}
